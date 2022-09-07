82b5c45eae24466e9ffc0d9d651d4a8ef923de53
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2022-09-07 06:32:30 +0530

    MWE!
    Signed-off-by: ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,37 +1,41 @@
-from uvloop import install
+from os import environ
+from asyncio import sleep
+from subprocess import PIPE, Popen
 
-install()
 from telegram import Update
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-
-pbot = Client(
-    "testbot",
-    api_id=6,
-    api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e",
-    bot_token="1809261258:AAHusgeYUgAcW6F1poVEscUD55Ty4_EULA0", # will be revoked!
-)
-
-
-@pbot.on_message(filters.command("start"))
-async def starth(_: pbot, m: Message):
-  await m.reply_text("Pyro")
-  return
+defaults = Defaults(block=False)
 
 
 async def start(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
-    user = update.effective_user
-    await update.message.reply_html("PTB")
+    await update.message.reply_text("PTB OP!")
+    # check aliveness!
     return
 
 
-application = Application.builder().token("1809261258:AAHusgeYUgAcW6F1poVEscUD55Ty4_EULA0").build()
+async def sleeper(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
+    a = await update.effective_message.reply_text("Starting ...")
+    await sleep(10)
+    await a.edit_text("Done!")
+    # this will work!
+    return
+
+
+async def sleeper2(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
+    a = await update.effective_message.reply_text("Starting non sync work ...")
+    Popen("sleep 10", stdout=PIPE, stderr=PIPE, shell=True)
+    await a.edit_text("Done!")
+    # this will stop everything! so we should what ?
+    return
+
+
+builder = Application.builder().token(environ.get("TOKEN")).defaults(defaults).concurrent_updates(True).build()
 application.add_handler(CommandHandler("start", start))
-print("Starting")
-pbot.start()
+application.add_handler(CommandHandler("work", sleeper))
+application.add_handler(CommandHandler("work1", sleeper2))
 print("Bot started")
-application.run_polling(close_loop=False)
-pbot.stop()
+application.run_polling()
 print("Bye")
