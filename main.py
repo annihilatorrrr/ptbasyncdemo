e94a397168edd313b705518cf3c8318f095d72be
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2022-09-07 06:50:43 +0530

    Update main.py
    Signed-off-by: ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>

--- main.py
+++ main.py
@@ -33,28 +33,10 @@ async def sleeper2(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     return
 
 
-async def looptask(m: Message):
-    process = Popen("sleep 10", stdout=PIPE, stderr=PIPE, shell=True)
-    stdout, stderr = process.communicate()
-    stdout = stdout.decode()
-    stderr = stderr.decode()
-    await m.edit_text("Done!")
-    return
-
-
-async def sleeper3(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
-    a = await update.effective_message.reply_text("Starting non sync work in loop task ...")
-    loop = get_event_loop()
-    loop.create_task(looptask(a))
-    # this will work!
-    return
-
-
 application = Application.builder().token(environ.get("TOKEN")).defaults(defaults).concurrent_updates(True).build()
 application.add_handler(CommandHandler("start", start))
 application.add_handler(CommandHandler("work", sleeper))
 application.add_handler(CommandHandler("work1", sleeper2))
-application.add_handler(CommandHandler("work2", sleeper3))
 print("Bot started")
 application.run_polling()
 print("Bye")
