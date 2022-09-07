3e3bdfd7c1ab9d5af6d4626e444f401586b90e57
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2022-09-07 06:44:40 +0530

    Hmm
    Signed-off-by: ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from os import environ
-from asyncio import sleep
+from asyncio import get_event_loop, sleep
 from subprocess import PIPE, Popen
 
 from telegram import Update
@@ -29,7 +29,24 @@ async def sleeper2(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     stdout = stdout.decode()
     stderr = stderr.decode()
     await a.edit_text("Done!")
-    # this will stop everything! so we should what ?
+    # this will stop everything! so what should we do ?
+    return
+
+
+async def looptask(m: Message):
+    process = Popen("sleep 10", stdout=PIPE, stderr=PIPE, shell=True)
+    stdout, stderr = process.communicate()
+    stdout = stdout.decode()
+    stderr = stderr.decode()
+    await m.edit_text("Done!")
+    return
+
+
+async def sleeper3(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
+    a = await update.effective_message.reply_text("Starting non sync work in loop task ...")
+    loop = get_event_loop()
+    loop.create_task(looptask(a))
+    # this will work!
     return
 
 
@@ -37,6 +54,7 @@ application = Application.builder().token(environ.get("TOKEN")).defaults(default
 application.add_handler(CommandHandler("start", start))
 application.add_handler(CommandHandler("work", sleeper))
 application.add_handler(CommandHandler("work1", sleeper2))
+application.add_handler(CommandHandler("work2", sleeper3))
 print("Bot started")
 application.run_polling()
 print("Bye")
